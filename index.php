1b87d2ac67453b6a72219eee30d57e68042c0ea4
Vedansh Kamdar <vedansh.kamdar1413@gmail.com>
2023-04-16 23:31:41 +0530

    changes done

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Boarding Pass Generator</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="hero-container">
+        <h1 class="hero-heading">Boarding Pass Generator</h1>
+        <p class="hero-paragraph">Generate your boarding pass instantly and travel hassle-free!</p>
+        <a href="client.php" class="hero-button">Grab your Boarding Pass here!</a>
+    </div>
+</body>
+</html>
